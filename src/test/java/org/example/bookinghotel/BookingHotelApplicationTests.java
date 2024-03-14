b7c55cff0a5e79a78d646d5f948a71c741d6279e
Ta Minh Tri <taminhtri20@gmail.com>
2024-03-14 15:21:33 +0700

    first commit

---
+++ src/test/java/org/example/bookinghotel/BookingHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookinghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
